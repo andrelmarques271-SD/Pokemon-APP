0906dc7b1c6c99a87be1efd7da03d14a0ac5bfbd
André Marques <andrelmarques271@gmail.com>
2026-04-02 13:23:44 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 let listaPokemon = []
-async function carregarPokemon() {    
-    let response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")    
-    let data = await response.json()    
-    listaPokemon = data.results    
-    if (listaPokemon.length === 0) {        
-        alert("Não achamos o seu Pokémon, tenta de novo!")        
-        return    }}   
 
-        carregarPokemon()
-
-function sortearPokemon(){    
-    let numero = Math.floor(Math.random() * listaPokemon.length)    
-    let pokemonEscolhido = listaPokemon[numero]    
-    buscarDetalhes(pokemonEscolhido.url)}
-
-    async function buscarDetalhes(url) {    
-    let response = await fetch(url)    
+async function carregarPokemon() {
+    let response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
     let data = await response.json()
-    
-    mostrarPokemon(data)}
+    listaPokemon = data.results
+    if (listaPokemon.length === 0) {
+        alert("Não achamos o seu Pokémon, tenta de novo!")
+        return
+    }
+}   
 
-    function mostrarPokemon(pokemon) {    
-    document.getElementById("pokemon").innerHTML = `    
-    <h2>${pokemon.name}</h2>    
-    <img src="${pokemon.sprites.front_default}" />        
-    <p>Altura: ${pokemon.height}</p>    
-    <p>Peso: ${pokemon.weight}</p>    `
+carregarPokemon()
+
+function sortearPokemon(){
+    let numero = Math.floor(Math.random() * listaPokemon.length)
+    let pokemonEscolhido = listaPokemon[numero]
+    buscarDetalhes(pokemonEscolhido.url)
+}
+
+async function buscarDetalhes(url) {
+    let response = await fetch(url)
+    let data = await response.json()
+
+    mostrarPokemon(data)
+}
+
+function mostrarPokemon(pokemon) {
+    document.getElementById("pokemon").innerHTML = `
+    <h2 tabindex="0">${pokemon.name}</h2>
+    <img src="${pokemon.sprites.front_default}" alt="Imagem do Pokémon ${pokemon.name}" />    
+    <p><strong>Altura:</strong> ${pokemon.height}</p>
+    <p><strong>Peso:</strong> ${pokemon.weight}</p>
+    `
+    if (navigator.vibrate) {
+        navigator.vibrate(200)
+    }
 }
---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = "pokemon-app-v1"
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json"
+]
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache)
+    })
+  )
+})
